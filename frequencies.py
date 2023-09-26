55ff704d616b46732ed0ae315a485a820bd8bd80
Vincent Ha <vincent.ha@kcl.ac.uk>
2023-09-26 12:47:27 +0100

    Complete frequencies function

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        if str(item) in frequencies.keys():
+            frequencies[str(item)] +=1
+        else:
+            frequencies[str(item)] = 1
+    
     # Your code goes here
     return frequencies
